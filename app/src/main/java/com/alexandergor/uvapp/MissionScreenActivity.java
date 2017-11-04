1f2d01aece46414bac4447b781f367155b9390ca
orn0t <ornot.work@gmail.com>
2017-11-04 14:42:44 +0200

    apply button fragment fixes

--- app/src/main/java/com/alexandergor/uvapp/MissionScreenActivity.java
+++ app/src/main/java/com/alexandergor/uvapp/MissionScreenActivity.java
@@ -1,8 +1,13 @@
 package com.alexandergor.uvapp;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,13 +29,23 @@ public class MissionScreenActivity extends AppCompatActivity {
 
         TextView missionTitle = (TextView) findViewById(R.id.missionTitle);
         TextView missionDescription = (TextView) findViewById(R.id.missionDescription);
-        ListView participantsList = (ListView) findViewById(R.id.missionParticipants);
-
 
         Log.i("Mission", item.participants.toString());
 
+        Fragment acctionApply = new MissionApplyButton();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.missionActionsHolder, acctionApply);
+        fragmentTransaction.commit();
+
         modelMissionParticipantListAdatper participantListAdatper = new modelMissionParticipantListAdatper(item.participants);
-        participantsList.setAdapter(participantListAdatper);
+
+        // TODO: 11/4/17  find better solution for listview inside scrollable
+        LinearLayout participantsList = (LinearLayout) findViewById(R.id.missionParticipants);
+        for (int i = 0; i < participantListAdatper.getCount(); i++) {
+            participantsList.addView(
+                    participantListAdatper.getView(i, null, participantsList)
+            );
+        }
 
         missionTitle.setText(item.title);
         missionDescription.setText(item.description);
