fd9af025a75602c28a924e23d53075f3ff9ad0d0
orn0t <ornot.work@gmail.com>
2017-11-09 01:58:39 +0200

    UVAPP-72. Products list, model, adapter, view

--- app/src/main/java/com/alexandergor/uvapp/TrophyScreenActivity.java
+++ app/src/main/java/com/alexandergor/uvapp/TrophyScreenActivity.java
@@ -1,13 +1,53 @@
 package com.alexandergor.uvapp;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.facebook.AccessToken;
+
+import java.util.List;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TrophyScreenActivity extends BaseBottomNavigationActivity {
+
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy_screen);
+
+        realm = Realm.getDefaultInstance();
+
+        String authToken = AccessToken.getCurrentAccessToken().getToken();
+        UVSApiClient api = UVSApp.getUvsApiClient(authToken);
+        Call<List<modelProduct>> reqProducts = api.listProducts();
+        reqProducts.enqueue(new Callback<List<modelProduct>>() {
+            @Override
+            public void onResponse(Call<List<modelProduct>> call, Response<List<modelProduct>> response) {
+                List<modelProduct> products = response.body();
+
+                realm.beginTransaction();
+                realm.copyToRealmOrUpdate(products);
+                realm.commitTransaction();
+            }
+
+            @Override
+            public void onFailure(Call<List<modelProduct>> call, Throwable t) {
+
+            }
+        });
+
+        ListView listView = (ListView) findViewById(R.id.allProductsList);
+        RealmResults<modelProduct> products = realm.where(modelProduct.class).findAllAsync();
+        modelProductListAdapter productListAdapter = new modelProductListAdapter(products);
+
+        listView.setAdapter(productListAdapter);
+
 
         initNavigation(R.id.action_trophy);
     }
